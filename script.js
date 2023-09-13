7578dc3b2bcb4c265868cd3cab602223cc1c88cf
academate <138305501+academate@users.noreply.github.com>
2023-09-13 20:21:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let dat = new Date();
 let curYear = dat.getFullYear();
 
 footerTxt.innerHTML = "Copyright " +  "&#169; " + curYear + ", AcadeMate. All Rights Reserved";
+
+let btnText = Array.from(document.getElementsByClassName("multipurpose-btn"));
+
+btnText.forEach(btn => {
+    btn.innerHTML = "Download";
+});
