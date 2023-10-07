c1b83a3be470460dea65c5a7b1ca6f8a08add8f9
vigneshmrx <vigneshsundrani55@gmail.com>
2023-10-07 22:50:04 +0530

    app_and_learn_pgm_page_update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let footerTxt = document.getElementsByClassName("copywrite-text")[0];
 let dat = new Date();
 let curYear = dat.getFullYear();
 
-footerTxt.innerHTML = "Copyright " +  "&#169; " + curYear + ", AcadeMate. All Rights Reserved";
+footerTxt.innerHTML = "Copyright " +  "&#169; " + curYear + ", AcadeMate. All Rights Reserved.";
 
 footerTxt.innerHTML += "<br><br>Email: academate@gmail.com"
 
@@ -23,8 +23,17 @@ window.addEventListener("scroll", () => {
 });
 
 //Loader page
-const loaderPage = document.getElementsByClassName("loader-page")[0];
 
-setTimeout(() => {
+
+// setTimeout(() => {
+    
+// }, 1520);
+
+// window.onload = () => {
+//     loaderPage.style.display = "none";
+// }
+
+const loaderRemoverFun = () => {
+    const loaderPage = document.getElementsByClassName("loader-page")[0];
     loaderPage.style.display = "none";
-}, 1520);
+}
